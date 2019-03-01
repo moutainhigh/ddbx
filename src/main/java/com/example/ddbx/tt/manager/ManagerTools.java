18b84b57b4fa825bca6968cb1cb145706f69eff6
SunXiangxin <1641772665@qq.com>
2019-03-01 10:53:08 +0800

    汽车评估完成

--- src/main/java/com/example/ddbx/tt/manager/ManagerTools.java
+++ src/main/java/com/example/ddbx/tt/manager/ManagerTools.java
@@ -26,7 +26,7 @@ public class ManagerTools {
     public static boolean checkCn(String cn) {
         String[] allowCnList = {"admin", "assess_admin", "home", "admin2", "button", "demo_upfile", "icon", "general",
                 "admin_agp", "Timeline", "Modals", "table", "comm_citys", "sys_modal", "fs_agp", "readme", "sys_error", "readmedev",
-                "fs","spmq",
+                "fs","spmq", "qcpg",
                 "gems", "dd_icbc", "zxcx", "alltask", "mytask", "my_job", "sys_config", "sys_config_son"}; // 允许的cn,只有在列表中的cn才允许访问
         return Tools.arrayIndexOf(allowCnList, cn);
     }
@@ -47,7 +47,7 @@ public class ManagerTools {
                 "fs",
                 "gems",
                 "dd_icbc",
-                "alltask", "zxcx","spmq",
+                "alltask", "zxcx","spmq", "qcpg",
 
                 "mytask", "sys_config", "sys_config_son"
         }; // 允许的cn,只有在列表中的cn才使用数据库，
@@ -116,6 +116,8 @@ public class ManagerTools {
                 return "zx_1";
             case "zxcx":
                 return "zxcx";
+            case "qcpg":
+                return "qcpg";
             // 不需要使用数据库的CN
             case "home":
             case "button":
