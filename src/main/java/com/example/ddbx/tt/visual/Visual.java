6921d56f5e8e0a52700be27456fd3c496e516c32
Qin57761 <577618800@qq.com>
2019-03-07 18:08:39 +0800

    2019-03-07提交  可视化数据后台控制器，前台显示

--- src/main/java/com/example/ddbx/tt/visual/Visual.java
+++ src/main/java/com/example/ddbx/tt/visual/Visual.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,23 +22,11 @@ import java.io.PrintWriter;
  * 用于显示可视化数据
  *
  */
+@RestController      //  默认类中的方法都会以 json 的格式返回
 @Controller
 public class Visual {
     DbCtrl dbCtrl;
-
-    @RequestMapping(value = "/manager/visual/select", method = RequestMethod.POST)
-    @ResponseBody
-    public String select(HttpServletRequest request, HttpServletResponse response){
-        String sql="select id,gems_id from dd_icbc";
-        TtList list=selectSQL(sql);
-        String [] s=new String[5];
-        s[0]=list.get(0).get("id");
-        s[1]=list.get(1).get("id");
-        s[2]=list.get(2).get("id");
-        JSONArray jsonArray = JSONArray.fromObject(s);
-        ToJson(request,response,jsonArray);
-        return null;
-    }
+    String [][] s;
 
     //输入sql语句进行查询操作
     public TtList selectSQL(String sql){
@@ -53,18 +42,61 @@ public class Visual {
         }
         return allCustomer;
     }
-    //转换成json字符串并输出到前台
-    public void ToJson(HttpServletRequest request, HttpServletResponse response,JSONArray jsonArray){
-        try {
-            response.setContentType("text/html;charset=UTF-8");
-            response.setContentType("application/json");
-            PrintWriter pw = null;
-            pw = response.getWriter();
-            pw.print(jsonArray); // 轨迹图条件，取少量数据
-            pw.flush();
-            pw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+
+    /**
+     * 使用 注解@RestController
+     *  使用注解不需要转换成json
+     *  默认类中的方法都会以 json 的格式返回
+     * */
+    @RequestMapping(value = "/manager/visual/select", method = RequestMethod.POST)
+    @ResponseBody
+    public String[][] select(){
+        String sql="select id,gems_id from dd_icbc";
+        TtList list=selectSQL(sql);
+        s=new String[5][5];
+        s[0][0]=list.get(0).get("id");
+        s[1][1]=list.get(1).get("id");
+        s[2][2]=list.get(2).get("id");
+        s[1][2]=list.get(1).get("gems_id");
+        s[2][3]=list.get(2).get("gems_id");
+        return s;
     }
+
+    /**
+     * 不使用 注解@RestController      //  默认类中的方法都会以 json 的格式返回
+     * 需要方法转换成json字符串然后传到前台
+     *
+     *   @RequestMapping(value = "/manager/visual/select1", method = RequestMethod.POST)
+     *   @ResponseBody
+     *   public String select1(HttpServletRequest request, HttpServletResponse response){
+     *       String sql="select id,gems_id from dd_icbc";
+     *       TtList list=selectSQL(sql);
+     *       String [][] s=new String[3][5];
+     *       s[0][0]=list.get(0).get("id");
+     *       s[0][1]=list.get(1).get("id");
+     *       s[1][2]=list.get(2).get("id");
+     *       s[2][1]=list.get(3).get("id");
+     *       s[2][2]=list.get(4).get("gems_id");
+     *       JSONArray jsonArray = JSONArray.fromObject(s);
+     *       ToJson(request,response,jsonArray);
+     *       return null;
+     *   }
+     *
+     *   //转换成json字符串并输出到前台
+     *   public void ToJson(HttpServletRequest request, HttpServletResponse response,JSONArray jsonArray){
+     *       try {
+     *           response.setContentType("text/html;charset=UTF-8");
+     *           response.setContentType("application/json");
+     *           PrintWriter pw = null;
+     *           pw = response.getWriter();
+     *           pw.print(jsonArray); // 轨迹图条件，取少量数据
+     *           pw.flush();
+     *           pw.close();
+     *       } catch (IOException e) {
+     *           e.printStackTrace();
+     *       }
+     *   }
+     */
+
 }
--- src/main/resources/static/manager/js/visual/visual.js
+++ src/main/resources/static/manager/js/visual/visual.js
@@ -10,88 +10,72 @@
             var summarydata = [];
             var timeline = [];
             var j;
-            alert(data[0],data[1]);
+            alert(111);
            /* for(var i=0;i<9;i++){
                 j=8-i;
                 timeline[i]=data[1][j]=null?'2019-'+i:data[1][j];
                 summarydata[i]=data[0][j]=null?'0':data[0][j];
             }*/
-    var option = {
-        tooltip : {
-            trigger: 'axis'
-        },
-
-        grid: {show:'true',borderWidth:'0'},//去除边框
-        legend: {
-            data:['蒸发量','降水量','平均温度'],
-            y:"bottom",
-        },
-        xAxis : [
-            {
-                type : 'category',
-                splitLine:{show: false},//去除网格线
-                data : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
-            }
-        ],
-        yAxis : [
-            {
-                type : 'value',
-                name : '水量',
-                splitLine:{show: false},//去除网格线
-                axisLabel : {
-                    formatter: '{value} ml'
+            var  option = {
+                tooltip : {
+                    trigger: 'axis'
                 },
-                axisTick:axistick,
-                axisLine :{symbol:['none', 'arrow'],symbolSize:['10', '13']}//轴线
-
-
-            },
-            {
-                type : 'value',
-                name : '温度',
-                splitLine:{show: false},//去除网格线
-                axisTick:axistick,
-                axisLine :{symbol:['none', 'arrow'],symbolSize:['10', '13']},//轴线
-                axisLabel : {
-                    formatter: '{value} °C'
-                }
-            }
-        ],
-        series : [
-
-            {
-                name:'蒸发量',
-                type:'bar',
-                itemStyle: {color:'rgb(025,156,222)'},
-                data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-            },
-            {
-                name:'降水量',
-                type:'bar',
-                itemStyle: {color:'rgb(168,156,169)'},//柱形颜色
-                data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
-            },
-            {
-                name:'平均温度',
-                type:'line',
-                yAxisIndex: 1,
-                // lineStyle: {
-                //          color:"#F7331E",
-                // },
-                /*折线拐点标志的样式。*/
-                itemStyle:{
-                    color:"#F7331E",         //颜色
-                    //borderType:"solid",  //柱条的描边类型，默认为实线，支持 'dashed', 'dotted'。
-                    //shadowBlur:10,       //图形阴影的模糊大小。
-                    //shadowColor:"#000"  //阴影颜色
+                grid: {show:'true',borderWidth:'0'},//去除边框
+                legend: {
+                    data:['蒸发量','降水量','平均温度'],
+                    y:"bottom"
                 },
-                data:[2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
-            }
-        ]
-    };
+                xAxis : [
+                    {
+                        splitLine:{show: false},//去除网格线
+                        type : 'category',
+                        data : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月']
+                    }
+                ],
+                yAxis : [
+                    {
+                        type : 'value',
+                        name : '水量',
+                        splitLine:{show: false},//去除网格线
+                        axisTick:axistick,
+                        axisLine :{symbol:['none', 'arrow'],symbolSize:['10', '13']},//轴线
+                        axisLabel : {
+                            formatter: '{value} ml'
+                        }
+                    },
+                    {
+                        type : 'value',
+                        name : '温度',
+                        splitLine:{show: false},//去除网格线
+                        axisTick:axistick,
+                        axisLine :{symbol:['none', 'arrow'],symbolSize:['10', '13']},//轴线
+                        axisLabel : {
+                            formatter: '{value} °C'
+                        }
+                    }
+                ],
+                series : [
 
+                    {
+                        name:'蒸发量',
+                        type:'bar',
+                        data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+                    },
+                    {
+                        name:'降水量',
+                        type:'bar',
+                        data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+                    },
+                    {
+                        name:'平均温度',
+                        type:'line',
+                        yAxisIndex: 1,
+                        data:[2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+                    }
+                ]
+            };
 
-    // 为echarts对象加载数据
+            // 为echarts对象加载数据
     myChart.setOption(option);
             //ajax结尾
         },
@@ -109,7 +93,7 @@
     $.ajax({
         dataType : "json",
         type : "post",
-        url : "visual/select",
+        url : "visual/select1",
         success : function(data) {
             var summarydata = [];
             var timeline = [];
